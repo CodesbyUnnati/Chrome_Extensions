21133b52fa24a78341144a283ba3b90c05b6ebfb
Unnati Mishra <54355445+CodesbyUnnati@users.noreply.github.com>
2021-09-30 23:29:28 +0530

    Delete script.js

--- Extension/script.js
+++
@@ -1,9 +0,0 @@
-fetch('https://icanhazdadjoke.com/slack')
-.then(data => data.json())
-.then(jokeData=>
-    {
-        const jokeText=jokeData.attachments[0].text;
-        const jokeElement=document.getElementById('jokeElement');
-
-        jokeElement.innerHTML=jokeText;
-    })
