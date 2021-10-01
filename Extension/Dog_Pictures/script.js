e6e12e1454a8690b3ded2a9b3365cfcf681874a3
Abhishek <76660005+abhishek213-alb@users.noreply.github.com>
2021-10-01 22:48:29 +0530

    Delete Extension directory

--- Extension/Dog_Pictures/script.js
+++
@@ -1,9 +0,0 @@
-fetch('https://random.dog/woof.json')
-.then(data => data.json())
-.then(DogData=>
-    {
-        const dogImages=DogData.url;
-        const dogElement=document.getElementById('dogElement');
-
-        dogElement.src=dogImages;
-    })
