4a047b6b2abf74c73295d6595d1017c8e4abc5a3
Vivek Kumar Gupta <63556497+thevirus69@users.noreply.github.com>
2021-10-01 10:19:10 +0530

    Add files via upload
    I have Added an Loading Bar 
    So now we dont have to wait for images , it will show loading bar , as soon as image fetched it will show the images of cute dogs .
    #Hacktoberfest

--- Extension/Dog_Pictures/script.js
+++ Extension/Dog_Pictures/script.js
@@ -1,7 +1,12 @@
+const spinner = document.getElementById("spinner");
+
+
+spinner.removeAttribute('hidden');
 fetch('https://random.dog/woof.json')
 .then(data => data.json())
 .then(DogData=>
     {
+        spinner.setAttribute('hidden', '');
         const dogImages=DogData.url;
         const dogElement=document.getElementById('dogElement');
 
