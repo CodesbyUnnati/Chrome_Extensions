809a198df2bf04bc0be269481736e0a717ba0974
rajat-mehra05 <rajat.m.5278@gmail.com>
2021-10-02 11:03:21 +0530

    made developer-color extension

--- Extension/dogs_gifs/script.js
+++
@@ -1,8 +0,0 @@
-fetch("https://random.dog/woof.json")
-  .then((data) => data.json())
-  .then((dogData) => {
-    let dogsPhotos = dogData.url;
-    let dogEl = document.getElementById("dogEl");
-
-    dogEl.src = dogsPhotos;
-  });
